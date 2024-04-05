0002d6e09122f9406cd6fe4770047b1b26ee7efc
goneal26 <g.oneal26@ncf.edu>
2024-04-04 20:19:42 -0400

    Implemented prompt mode startup

---
+++ cpongo/scanner.c
@@ -0,0 +1,14 @@
+//
+// Created by Tre on 4/2/2024.
+//
+
+#include "scanner.h"
+
+enum TokenType {
+    // single-character tokens
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    LEFT_BRACKET, RIGHT_BRACKET, COMMA, MINUS,
+    PLUS, SEMICOLON, SLASH, STAR,
+
+
+};
---
+++ cpongo/scanner.h
@@ -0,0 +1,8 @@
+//
+// Created by Tre on 4/2/2024.
+//
+
+#ifndef CPONGO_SCANNER_H
+#define CPONGO_SCANNER_H
+
+#endif //CPONGO_SCANNER_H
